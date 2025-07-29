5b734b5e7cd927c46446ac94757c699dfebba556
Débora Suzuki <debora.suzuki@llzgarantidora.com.br>
2025-07-29 15:34:35 -0300

    feat: implementar componente de navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,22 @@
 <template>
-  <div>
-    <p>This usage default.layout</p>
+  <div class="bg-[var(--color-neutral-99)] min-h-screen">
+    <LayoutNavbar />
+
     <NuxtPage />
   </div>
 </template>
 
 <script setup lang="ts">
-
 import { definePageMeta } from '#imports';
-import { useAuthStore } from '~/stores/auth'
+import { useAuthStore } from '~/stores/auth';
 
-const authStore = useAuthStore()
+const authStore = useAuthStore();
 
 if (!authStore.isAuthenticated) {
-  navigateTo('/login')
+  navigateTo('/login');
 }
 
 definePageMeta({
-  middleware: 'auth'
-})
-
-</script> 
+  middleware: 'auth',
+});
+</script>
